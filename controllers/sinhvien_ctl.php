5b81319228a6f65484aaf67f4cda53d151baa6c0
PhucLe03 <hailua112003@gmail.com>
2023-12-04 17:28:18 +0700

    Student change password

--- controllers/sinhvien_ctl.php
+++ controllers/sinhvien_ctl.php
@@ -45,29 +45,6 @@ function usernameDocNhat($uname, $conn, $masinhvien = 0)
   }
 }
 
-
-function XacMinhMatKhauSinhVien($sinhvien_mk, $conn, $masinhvien)
-{
-  $sql = "SELECT * FROM sinhvien
-           WHERE masinhvien=?";
-  $stmt = $conn->prepare($sql);
-  $stmt->execute([$masinhvien]);
-
-  if ($stmt->rowCount() == 1) {
-    $sinhvien = $stmt->fetch();
-    $pass  = $sinhvien['matkhau'];
-
-    if (password_verify($sinhvien_mk, $pass)) {
-      return 1;
-    } else {
-      return 0;
-    }
-  } else {
-    return 0;
-  }
-}
-
-
 function getInfoSV($id, $conn) {
   $sql = "SELECT * FROM in4sinhvien
            WHERE masinhvien=?";
